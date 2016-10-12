834343bbd760f1e31b2b4f7759fbb185916d04e4
gaoboallen <bg447@cornell.edu>
2016-10-11 20:20:43 -0400

    get hot searches

--- testpytrends.py
+++ testpytrends.py
@@ -1,12 +1,13 @@
 # @Author: Gao Bo
 # @Date:   2016-10-03T22:39:11-04:00
 # @Last modified by:   Gao Bo
-# @Last modified time: 2016-10-04T13:47:00-04:00
+# @Last modified time: 2016-10-11T20:20:03-04:00
 
 
 
 from pytrends.request import *
 import json
+import xmltodict
 
 def parsetop30in30(trends):
     tdate = 0
@@ -26,7 +27,13 @@ def parsetop30in30(trends):
     #     print()
 
 def parseHottrends(trends):
-    print(type(trends))
+    trendsList = trends["rss"]["channel"]["item"]
+    keywordsList = []
+    # tLen = len(trendsList)
+    assert len(trendsList) == 20
+    for tItem in trendsList:
+        keywordsList.append(tItem["title"])
+    return keywordsList
 
 
 if __name__ == "__main__":
@@ -36,10 +43,14 @@ if __name__ == "__main__":
 
     # top30 = pytrends.top30in30()
     # parsetop30in30(top30)
+    # hottrendsdetail = pytrends.hottrends({})
 
-    hottrendsdetail = pytrends.hottrendsdetail({})
-    parseHottrends(hottrendsdetail)
+    # get trending searches now
+    hottrendsdetail = xmltodict.parse(pytrends.hottrendsdetail({}))
+    keywordsList = parseHottrends(hottrendsdetail)
 
-    outfile = open('trendsOutput.txt', 'w')
+    print(keywordsList)
+
+    outfile = open('trendsOutput2.txt', 'w')
     outfile.write(json.dumps(hottrendsdetail, sort_keys=True, indent=4, separators=(',', ': ')))
     outfile.close()
